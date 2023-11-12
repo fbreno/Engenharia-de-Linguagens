e21473c45f1f8f141cfeb2c29d2d83b56dde39f5
Fernando <fernandobrenofx@gmail.com>
2023-11-12 15:07:27 -0300

    Correção arquivo de run

--- run.sh
+++ run.sh
@@ -9,53 +9,40 @@ fi
 # Parâmetro de entrada fornecido
 parametro=$1
 
+# Caminho para o diretório lexico
+lexico_dir="lexico"
+
+# Caminho para o diretório sintatico
+sintatico_dir="sintatico"
+
+# Nome do arquivo de link
+link_file="run_link"
+
 # Verifica se o diretório lexico existe
-if [ -d "lexico" ]; then
-    echo "Entrando no diretório lexico..."
-    cd lexico
-
-    # Verifica se lex.l existe
-    if [ -f "lex.l" ]; then
-        echo "Executando lex no arquivo lex.l"
-        lex lex.l
-    else
-        echo "O arquivo lex.l não foi encontrado."
-        exit 1
-    fi
-
-    # Retorna ao diretório anterior
-    cd ..
-
-    # Verifica se o diretório sintatico existe
-    if [ -d "sintatico" ]; then
-        echo "Entrando no diretório sintatico..."
-        cd sintatico
-
-        # Verifica se parser.y existe
-        if [ -f "parser.y" ]; then
-            echo "Executando yacc no arquivo parser.y com os argumentos -d -v -g"
-            yacc -d -v -g parser.y
-
-            # Compila os arquivos gerados por lex e yacc
-            echo "Compilando os arquivos gerados por lex e yacc..."
-            gcc lex.yy.c y.tab.c -o run.o
-
-            # Executa o programa compilado com o parâmetro fornecido
-            echo "Executando ./run.o com o parâmetro: $parametro"
-            ./run.o "$parametro"
-        else
-            echo "O arquivo parser.y não foi encontrado."
-            exit 1
-        fi
-
-        # Retorna ao diretório anterior
-        cd ..
-    else
-        echo "O diretório sintatico não existe."
-        exit 1
-    fi
-
+if [ -d "$lexico_dir" ]; then
+    echo "Executando lex no arquivo $lexico_dir/lex.l"
+    lex $lexico_dir/lex.l
 else
-    echo "O diretório lexico não existe."
+    echo "O diretório $lexico_dir não existe."
+    exit 1
+fi
+
+# Verifica se o diretório sintatico existe
+if [ -d "$sintatico_dir" ]; then
+    echo "Executando yacc no arquivo $sintatico_dir/parser.y com os argumentos -d -v -g"
+    yacc -d -v -g $sintatico_dir/parser.y
+
+    # Compila os arquivos gerados por lex e yacc
+    echo "Compilando os arquivos gerados por lex e yacc..."
+    gcc lex.yy.c y.tab.c -o $link_file
+
+    # Cria um arquivo de link na pasta raiz
+    ln -s $sintatico_dir/$link_file ./$link_file
+
+    # Executa o programa compilado com o parâmetro fornecido
+    echo "Executando ./$link_file com o parâmetro: $parametro"
+    ./$link_file "$parametro"
+else
+    echo "O diretório $sintatico_dir não existe."
     exit 1
 fi
